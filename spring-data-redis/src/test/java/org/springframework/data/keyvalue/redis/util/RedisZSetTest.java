6785a9c0176e80b0dd7c546ca311dfe0b9cdd928
Costin Leau <cleau@vmware.com>
2010-11-26 13:40:07 +0200

    + add zset tests

---
+++ spring-data-redis/src/test/java/org/springframework/data/keyvalue/redis/util/RedisZSetTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.keyvalue.redis.util;
+
+import org.springframework.data.keyvalue.redis.core.RedisTemplate;
+
+/**
+ * Parameterized instance of Redis sorted set tests.
+ * 
+ * @author Costin Leau
+ */
+public class RedisZSetTest extends AbstractRedisZSetTest<Object> {
+
+	/**
+	 * Constructs a new <code>RedisZSetTest</code> instance.
+	 *
+	 * @param factory
+	 * @param template
+	 */
+	public RedisZSetTest(ObjectFactory<Object> factory, RedisTemplate template) {
+		super(factory, template);
+	}
+
+	@Override
+	RedisStore<Object> copyStore(RedisStore<Object> store) {
+		return new DefaultRedisZSet(store.getKey().toString(), store.getOperations());
+	}
+
+	@Override
+	AbstractRedisCollection<Object> createCollection() {
+		String redisName = getClass().getName();
+		return new DefaultRedisZSet(redisName, template);
+	}
+}
