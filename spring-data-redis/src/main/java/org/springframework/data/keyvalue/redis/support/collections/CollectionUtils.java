cd0dcf9b137a032a6b267ee574d610fe49de2e44
Costin Leau <cleau@vmware.com>
2011-03-15 23:53:26 +0200

    + extracted cas rename logic into utility class

--- spring-data-redis/src/main/java/org/springframework/data/keyvalue/redis/support/collections/CollectionUtils.java
+++ spring-data-redis/src/main/java/org/springframework/data/keyvalue/redis/support/collections/CollectionUtils.java
@@ -20,6 +20,10 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.keyvalue.redis.core.RedisOperations;
+import org.springframework.data.keyvalue.redis.core.SessionCallback;
+
 /**
  * Utility class used mainly for type conversion by the default collection implementations.
  * Meant for internal use.
@@ -47,5 +51,56 @@ abstract class CollectionUtils {
 		}
 
 		return keys;
+	}
+
+	static <K> void rename(final K key, final K newKey, RedisOperations<K, ?> operations) {
+		operations.execute(new SessionCallback<Object>() {
+			@SuppressWarnings("unchecked")
+			@Override
+			public Object execute(RedisOperations operations) throws DataAccessException {
+				do {
+					operations.watch(key);
+
+					if (operations.hasKey(key)) {
+						operations.multi();
+						operations.rename(key, newKey);
+					}
+					else {
+						operations.multi();
+					}
+				} while (operations.exec() == null);
+				return null;
+			}
+		});
+	}
+
+	static <K> Boolean renameIfAbsent(final K key, final K newKey, RedisOperations<K, ?> operations) {
+		return operations.execute(new SessionCallback<Boolean>() {
+			@Override
+			public Boolean execute(RedisOperations operations) throws DataAccessException {
+				List<Object> exec = null;
+				do {
+					operations.watch(key);
+
+					if (operations.hasKey(key)) {
+						operations.multi();
+						operations.renameIfAbsent(key, newKey);
+					}
+					else {
+						operations.watch(newKey);
+						operations.multi();
+						operations.hasKey(newKey);
+						operations.hasKey(newKey);
+					}
+					exec = operations.exec();
+				} while (exec == null);
+
+				boolean result = ((Long) exec.get(0) == 1);
+				if (exec.size() > 1) {
+					result = !result;
+				}
+				return result;
+			}
+		});
 	}
 }
