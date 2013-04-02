5bd03c0aa50e07f84d7c526862cd50ac45c11c10
Jennifer Hickey <jhickey@vmware.com>
2013-04-02 16:24:06 -0700

    Ignore irrelevant tests of blocking ops in transactions

--- src/test/java/org/springframework/data/redis/connection/jedis/JedisConnectionTransactionIntegrationTests.java
+++ src/test/java/org/springframework/data/redis/connection/jedis/JedisConnectionTransactionIntegrationTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.redis.connection.jedis;
 
 import java.util.Arrays;
@@ -5,13 +20,18 @@ import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.data.redis.RedisSystemException;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("JedisConnectionIntegrationTests-context.xml")
+/**
+ * Integration test of {@link JedisConnection} transaction functionality.
+ * <p>
+ * Each method of {@link JedisConnection} behaves differently if executed with a
+ * transaction (i.e. between multi and exec or discard calls), so this test
+ * covers those branching points
+ *
+ * @author Jennifer Hickey
+ *
+ */
 public class JedisConnectionTransactionIntegrationTests extends
 		JedisConnectionPipelineIntegrationTests {
 
@@ -29,6 +49,37 @@ public class JedisConnectionTransactionIntegrationTests extends
 
 	@Ignore
 	public void testWatch() {
+	}
+
+	/*
+	 * Using blocking ops inside a tx does not make a lot of sense as it would
+	 * require blocking the entire server in order to execute the block
+	 * atomically, which in turn does not allow other clients to perform a push
+	 * operation. *
+	 */
+
+	@Ignore
+	public void testBLPop() {
+	}
+
+	@Ignore
+	public void testBRPop() {
+	}
+
+	@Ignore
+	public void testBRPopLPush() {
+	}
+
+	@Ignore
+	public void testBLPopTimeout() {
+	}
+
+	@Ignore
+	public void testBRPopTimeout() {
+	}
+
+	@Ignore
+	public void testBRPopLPushTimeout() {
 	}
 
 	// Unsupported Ops
