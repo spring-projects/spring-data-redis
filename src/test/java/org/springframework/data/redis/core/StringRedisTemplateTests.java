3244981a00945246e53622bf45ea4151d812e6f5
Jennifer Hickey <jhickey@gopivotal.com>
2013-07-16 13:40:18 -0700

    Fix StringRedisConnection not passed to callbacks in StringTemplate.execute()
    DATAREDIS-222

---
+++ src/test/java/org/springframework/data/redis/core/StringRedisTemplateTests.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.StringRedisConnection;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * Integration test of {@link StringRedisTemplate}
+ *
+ * @author Jennifer Hickey
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class StringRedisTemplateTests {
+
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+
+	@After
+	public void tearDown() {
+		redisTemplate.execute(new RedisCallback<Object>() {
+			public Object doInRedis(RedisConnection connection) {
+				connection.flushDb();
+				return null;
+			}
+		});
+	}
+
+	@Test
+	public void testKeys() throws Exception {
+		redisTemplate.opsForValue().set("foo", "bar");
+		assertNotNull(redisTemplate.keys("*"));
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Test(expected = IllegalArgumentException.class)
+	public void testTemplateNotInitialized() throws Exception {
+		RedisTemplate tpl = new RedisTemplate();
+		tpl.setConnectionFactory(redisTemplate.getConnectionFactory());
+		tpl.exec();
+	}
+
+	@Test
+	public void testStringTemplateExecutesWithStringConn() {
+		String value = redisTemplate.execute(new RedisCallback<String>() {
+			public String doInRedis(RedisConnection connection) {
+				StringRedisConnection stringConn = (StringRedisConnection) connection;
+				stringConn.set("test", "it");
+				return stringConn.get("test");
+			}
+		});
+		assertEquals(value,"it");
+	}
+}
