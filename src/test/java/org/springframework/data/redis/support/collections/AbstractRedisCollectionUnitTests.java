5b2781377d94fcda1894493ce4cf2012fcf95d09
Thomas Darimont <tdarimont@gopivotal.com>
2014-02-03 22:29:33 +0100

    DATAREDIS-188 - Infinite loop renaming a non-existent Collection when using Lettuce.
    We run into an infinite loop in org.springframework.data.redis.support.collections.CollectionUtils.rename(K, K, RedisOperations<K, ?>) if no value was associated with the key which leads to operations.hasKey(...) always returning false which prevents the actual renaming. RedisCollections will only send rename command if values are present as empty collections cannot be stored and therefor cannot be renamed.
    
    Original pull request: #28

---
+++ src/test/java/org/springframework/data/redis/support/collections/AbstractRedisCollectionUnitTests.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.support.collections;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author Christoph Strobl
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractRedisCollectionUnitTests {
+
+	private AbstractRedisCollection<String> collection;
+
+	@SuppressWarnings("rawtypes")//
+	private RedisTemplate redisTemplateSpy;
+	private @Mock RedisConnectionFactory connectionFactoryMock;
+	private @Mock RedisConnection connectionMock;
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Before
+	public void setUp() {
+
+		redisTemplateSpy = spy(new RedisTemplate() {
+
+			public Boolean hasKey(Object key) {
+				return false;
+			}
+		});
+		redisTemplateSpy.setConnectionFactory(connectionFactoryMock);
+		redisTemplateSpy.afterPropertiesSet();
+
+		collection = new AbstractRedisCollection<String>("key", redisTemplateSpy) {
+
+			private List<String> delegate = new ArrayList<String>();
+
+			@Override
+			public boolean add(String value) {
+				return this.delegate.add(value);
+			};
+
+			@Override
+			public DataType getType() {
+				return DataType.LIST;
+			}
+
+			@Override
+			public Iterator<String> iterator() {
+				return this.delegate.iterator();
+			}
+
+			@Override
+			public int size() {
+				return this.delegate.size();
+			}
+
+			@Override
+			public boolean isEmpty() {
+				return this.delegate.isEmpty();
+			}
+
+		};
+
+		when(connectionFactoryMock.getConnection()).thenReturn(connectionMock);
+	}
+
+	/**
+	 * @see DATAREDIS-188
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testRenameOfEmptyCollectionShouldNotTriggerRedisOperation() {
+
+		collection.rename("new-key");
+		verify(redisTemplateSpy, never()).rename(eq("key"), eq("new-key"));
+	}
+
+	/**
+	 * @see DATAREDIS-188
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testRenameCollectionShouldTriggerRedisOperation() {
+
+		when(redisTemplateSpy.hasKey(anyObject())).thenReturn(Boolean.TRUE);
+
+		collection.add("spring-data-redis");
+		collection.rename("new-key");
+		verify(redisTemplateSpy, times(1)).rename(eq("key"), eq("new-key"));
+	}
+}
