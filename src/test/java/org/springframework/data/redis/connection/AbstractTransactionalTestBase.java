4ef7fdcbf50e8555da74a88d055f796c6baa26e3
Christoph Strobl <cstrobl@gopivotal.com>
2014-03-20 14:55:24 +0100

    DATAREDIS-73 - Add support for spring managed transactions.
    RedisTemplate now supports enabling of transaction support, which is disabled by default.
    
    In case of enabled transaction support the RedisConnections will be bound to the current thread during ongoing outer transactions. We call MULTI at the beginning and depending on transaction state EXEC or DISCARD at its end.
    
    To support read operations during the transaction we wrap a proxy around the bound connection piping read operations to a new (non thread bound) connection obtained by the underlying RedisConnectionFactory.
    
    Transaction support is available for jedis, lettuce and srp, while had to be skipped for jredis due to the lack of support for MULTI.
    
    Original pull request: #64.

---
+++ src/test/java/org/springframework/data/redis/connection/AbstractTransactionalTestBase.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@TransactionConfiguration(transactionManager = "transactionManager")
+public abstract class AbstractTransactionalTestBase {
+
+	@Configuration
+	public abstract static class RedisContextConfiguration {
+
+		@Bean
+		public StringRedisTemplate redisTemplate() {
+
+			StringRedisTemplate template = new StringRedisTemplate(redisConnectionFactory());
+
+			// explicitly enable transaction support
+			template.setEnableTransactionSupport(true);
+			return template;
+		}
+
+		@Bean
+		public abstract RedisConnectionFactory redisConnectionFactory();
+
+		@Bean
+		public PlatformTransactionManager transactionManager() throws SQLException {
+			return new DataSourceTransactionManager(dataSource());
+		}
+
+		@Bean
+		public DataSource dataSource() throws SQLException {
+
+			DataSource ds = Mockito.mock(DataSource.class);
+			Mockito.when(ds.getConnection()).thenReturn(Mockito.mock(Connection.class));
+			return ds;
+		}
+	}
+
+	private @Autowired StringRedisTemplate template;
+
+	private @Autowired RedisConnectionFactory factory;
+
+	private List<String> KEYS = Arrays.asList("spring", "data", "redis");
+	private boolean valuesShouldHaveBeenPersisted = false;
+
+	@Before
+	public void setUp() {
+		valuesShouldHaveBeenPersisted = false;
+		cleanDataStore();
+	}
+
+	private void cleanDataStore() {
+
+		RedisConnection connection = factory.getConnection();
+		connection.flushDb();
+		connection.close();
+	}
+
+	@AfterTransaction
+	public void verifyTransactionResult() {
+
+		RedisConnection connection = factory.getConnection();
+		for (String key : KEYS) {
+			Assert.assertThat("Values for " + key + " should " + (valuesShouldHaveBeenPersisted ? "" : "NOT ")
+					+ "have been found.", connection.exists(key.getBytes()), Is.is(valuesShouldHaveBeenPersisted));
+		}
+		connection.close();
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@Rollback(true)
+	@Test
+	public void valueOperationSetShouldBeRolledBackCorrectly() {
+
+		for (String key : KEYS) {
+			template.opsForValue().set(key, key + "-value");
+		}
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@Rollback(false)
+	@Test
+	public void valueOperationSetShouldBeCommittedCorrectly() {
+
+		this.valuesShouldHaveBeenPersisted = true;
+		for (String key : KEYS) {
+			template.opsForValue().set(key, key + "-value");
+		}
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@Rollback(true)
+	@Test
+	public void listOperationLPushShoudBeRolledBackCorrectly() {
+
+		for (String key : KEYS) {
+			template.opsForList().leftPushAll(key, (String[]) KEYS.toArray());
+		}
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@Rollback(false)
+	@Test
+	public void listOperationLPushShoudBeCommittedCorrectly() {
+
+		this.valuesShouldHaveBeenPersisted = true;
+		for (String key : KEYS) {
+			template.opsForList().leftPushAll(key, (String[]) KEYS.toArray());
+		}
+	}
+}
---
+++ src/test/java/org/springframework/data/redis/core/ConnectionSplittingInterceptorUnitTests.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.core;
+
+import java.lang.reflect.Method;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisConnectionUtils.ConnectionSplittingInterceptor;
+import org.springframework.util.ClassUtils;
+
+/**
+ * @author Christoph Strobl
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConnectionSplittingInterceptorUnitTests {
+
+	private static final Method WRITE_METHOD, READONLY_METHOD;
+
+	private ConnectionSplittingInterceptor interceptor;
+
+	private @Mock RedisConnectionFactory connectionFactoryMock;
+
+	private @Mock RedisConnection freshConnectionMock;
+
+	private @Mock RedisConnection boundConnectionMock;
+
+	static {
+		try {
+			WRITE_METHOD = ClassUtils.getMethod(RedisConnection.class, "expire", byte[].class, long.class);
+			READONLY_METHOD = ClassUtils.getMethod(RedisConnection.class, "keys", byte[].class);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Before
+	public void setUp() {
+		interceptor = new ConnectionSplittingInterceptor(connectionFactoryMock);
+		Mockito.when(connectionFactoryMock.getConnection()).thenReturn(freshConnectionMock);
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@Test
+	public void interceptorShouldRequestFreshConnectionForReadonlyCommand() throws Throwable {
+
+		interceptor.intercept(boundConnectionMock, READONLY_METHOD, new Object[] { new byte[] {} }, null);
+		Mockito.verify(connectionFactoryMock, Mockito.times(1)).getConnection();
+		Mockito.verifyZeroInteractions(boundConnectionMock);
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@Test
+	public void interceptorShouldUseBoundConnectionForWriteOperations() throws Throwable {
+
+		interceptor.intercept(boundConnectionMock, WRITE_METHOD, new Object[] { new byte[] {}, 0L }, null);
+		Mockito.verify(boundConnectionMock, Mockito.times(1)).expire(Matchers.any(byte[].class), Matchers.anyLong());
+		Mockito.verifyZeroInteractions(connectionFactoryMock);
+	}
+
+	/**
+	 * @see DATAREDIS-73
+	 */
+	@SuppressWarnings("unchecked")
+	@Test(expected = InvalidDataAccessApiUsageException.class)
+	public void interceptorShouldNotWrapException() throws Throwable {
+
+		Mockito.when(freshConnectionMock.keys(Mockito.any(byte[].class))).thenThrow(
+				InvalidDataAccessApiUsageException.class);
+		interceptor.intercept(boundConnectionMock, READONLY_METHOD, new Object[] { new byte[] {} }, null);
+	}
+
+}
