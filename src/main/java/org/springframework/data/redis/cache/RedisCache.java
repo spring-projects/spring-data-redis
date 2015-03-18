96e98b709315a60af11716833d7b26e7d50d9d12
xiaomingchen <xiaomingchen@users.noreply.github.com>
2015-03-19 00:54:57 +0800

    Update RedisCache.java
    if use prefix ,cache name keys and cachename lock should add prefix too

--- src/main/java/org/springframework/data/redis/cache/RedisCache.java
+++ src/main/java/org/springframework/data/redis/cache/RedisCache.java
@@ -239,10 +239,10 @@ public class RedisCache implements Cache {
 			this.keyPrefix = keyPrefix;
 
 			StringRedisSerializer stringSerializer = new StringRedisSerializer();
-
+			
 			// name of the set holding the keys
-			this.setOfKnownKeys = usesKeyPrefix() ? new byte[] {} : stringSerializer.serialize(cacheName + "~keys");
-			this.cacheLockName = stringSerializer.serialize(cacheName + "~lock");
+			this.setOfKnownKeys = usesKeyPrefix() ? stringSerializer.serialize(new String(keyPrefix) + cacheName + "~keys"): stringSerializer.serialize(cacheName + "~keys");
+			this.cacheLockName = usesKeyPrefix() ? stringSerializer.serialize(new String(keyPrefix) + cacheName + "~lock"):stringSerializer.serialize(cacheName + "~lock");
 		}
 
 		/**
