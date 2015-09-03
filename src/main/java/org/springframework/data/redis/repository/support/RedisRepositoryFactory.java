d6eab7736933e8a0805dd6335eaa713edda372dd
Christoph Strobl <cstrobl@pivotal.io>
2015-09-03 09:19:23 +0200

    DATAREDIS-425 - Add dedicated RedisRepositoryFactory.
    We need to have a dedicated RepositoryFactory to be able to tweak query creation. Now derived queries are freshly instantiated new for every execution.

---
+++ src/main/java/org/springframework/data/redis/repository/support/RedisRepositoryFactory.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.repository.support;
+
+import java.lang.reflect.Method;
+
+import org.springframework.data.keyvalue.core.KeyValueOperations;
+import org.springframework.data.keyvalue.repository.query.KeyValuePartTreeQuery;
+import org.springframework.data.keyvalue.repository.query.KeyValuePartTreeQuery.QueryInitialization;
+import org.springframework.data.keyvalue.repository.support.KeyValueRepositoryFactory;
+import org.springframework.data.repository.core.NamedQueries;
+import org.springframework.data.repository.core.RepositoryMetadata;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+import org.springframework.data.repository.query.EvaluationContextProvider;
+import org.springframework.data.repository.query.QueryLookupStrategy;
+import org.springframework.data.repository.query.QueryLookupStrategy.Key;
+import org.springframework.data.repository.query.QueryMethod;
+import org.springframework.data.repository.query.RepositoryQuery;
+import org.springframework.data.repository.query.parser.AbstractQueryCreator;
+import org.springframework.util.Assert;
+
+/**
+ * {@link RepositoryFactorySupport} specific of handing Redis
+ * {@link org.springframework.data.keyvalue.repository.KeyValueRepository}.
+ * 
+ * @author Christoph Strobl
+ * @since 1.7
+ */
+public class RedisRepositoryFactory extends KeyValueRepositoryFactory {
+
+	/**
+	 * @param keyValueOperations
+	 * @see KeyValueRepositoryFactory#KeyValueRepositoryFactory(KeyValueOperations)
+	 */
+	public RedisRepositoryFactory(KeyValueOperations keyValueOperations) {
+		super(keyValueOperations);
+	}
+
+	/**
+	 * @param keyValueOperations
+	 * @param queryCreator
+	 * @see KeyValueRepositoryFactory#KeyValueRepositoryFactory(KeyValueOperations, Class)
+	 */
+	public RedisRepositoryFactory(KeyValueOperations keyValueOperations,
+			Class<? extends AbstractQueryCreator<?, ?>> queryCreator) {
+		super(keyValueOperations, queryCreator);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.keyvalue.repository.support.KeyValueRepositoryFactory#getQueryLookupStrategy(org.springframework.data.repository.query.QueryLookupStrategy.Key, org.springframework.data.repository.query.EvaluationContextProvider)
+	 */
+	@Override
+	protected QueryLookupStrategy getQueryLookupStrategy(Key key, EvaluationContextProvider evaluationContextProvider) {
+		return new RedisQueryLookupStrategy(key, evaluationContextProvider, getKeyValueOperations(), getQueryCreator());
+	}
+
+	/**
+	 * @author Christoph Strobl
+	 */
+	private static class RedisQueryLookupStrategy implements QueryLookupStrategy {
+
+		private EvaluationContextProvider evaluationContextProvider;
+		private KeyValueOperations keyValueOperations;
+
+		private Class<? extends AbstractQueryCreator<?, ?>> queryCreator;
+
+		/**
+		 * Creates a new {@link RedisQueryLookupStrategy} for the given {@link Key}, {@link EvaluationContextProvider},
+		 * {@link KeyValueOperations} and query creator type.
+		 * <p>
+		 * 
+		 * @param key
+		 * @param evaluationContextProvider must not be {@literal null}.
+		 * @param keyValueOperations must not be {@literal null}.
+		 * @param queryCreator must not be {@literal null}.
+		 */
+		public RedisQueryLookupStrategy(Key key, EvaluationContextProvider evaluationContextProvider,
+				KeyValueOperations keyValueOperations, Class<? extends AbstractQueryCreator<?, ?>> queryCreator) {
+
+			Assert.notNull(evaluationContextProvider, "EvaluationContextProvider must not be null!");
+			Assert.notNull(keyValueOperations, "KeyValueOperations must not be null!");
+			Assert.notNull(queryCreator, "Query creator type must not be null!");
+
+			this.evaluationContextProvider = evaluationContextProvider;
+			this.keyValueOperations = keyValueOperations;
+			this.queryCreator = queryCreator;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see org.springframework.data.repository.query.QueryLookupStrategy#resolveQuery(java.lang.reflect.Method, org.springframework.data.repository.core.RepositoryMetadata, org.springframework.data.repository.core.NamedQueries)
+		 */
+		public RepositoryQuery resolveQuery(Method method, RepositoryMetadata metadata, NamedQueries namedQueries) {
+
+			QueryMethod queryMethod = new QueryMethod(method, metadata);
+			KeyValuePartTreeQuery partTreeQuery = new KeyValuePartTreeQuery(queryMethod, evaluationContextProvider,
+					this.keyValueOperations, this.queryCreator);
+			partTreeQuery.setQueryIntialization(QueryInitialization.NEW);
+			return partTreeQuery;
+		}
+	}
+}
---
+++ src/main/java/org/springframework/data/redis/repository/support/RedisRepositoryFactoryBean.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.repository.support;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.data.keyvalue.repository.support.KeyValueRepositoryFactoryBean;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+
+/**
+ * Adapter for Springs {@link FactoryBean} interface to allow easy setup of {@link RedisRepositoryFactory} via Spring
+ * configuration.
+ * 
+ * @author Christoph Strobl
+ * @param <T> The repository type.
+ * @param <S> The repository domain type.
+ * @param <ID> The repository id type.
+ */
+public class RedisRepositoryFactoryBean<T extends Repository<S, ID>, S, ID extends Serializable> extends
+		KeyValueRepositoryFactoryBean<T, S, ID> {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.repository.core.support.RepositoryFactoryBeanSupport#createRepositoryFactory()
+	 */
+	@Override
+	protected RepositoryFactorySupport createRepositoryFactory() {
+		return new RedisRepositoryFactory(getOperations(), getQueryCreator());
+	}
+}
