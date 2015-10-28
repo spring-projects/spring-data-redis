97aef6edc0e064961342b9df831514b694affeaa
Christoph Strobl <cstrobl@pivotal.io>
2015-10-28 11:16:32 +0100

    DATAREDIS-425 - Simplify usage of RedisCallback for retrieving values.
    RedisCallbacks now be directly used for eg. accessing index values by key to retrieve a set of domain types by ids.

---
+++ src/test/java/org/springframework/data/redis/core/RedisKeyValueTemplateTests.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.core;
+
+import static org.hamcrest.core.Is.*;
+import static org.hamcrest.core.IsCollectionContaining.*;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.ConnectionFactoryTracker;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.mapping.RedisMappingContext;
+import org.springframework.util.ObjectUtils;
+
+/**
+ * @author Christoph Strobl
+ */
+@RunWith(Parameterized.class)
+public class RedisKeyValueTemplateTests {
+
+	RedisConnectionFactory connectionFactory;
+	RedisKeyValueTemplate template;
+	RedisTemplate<Object, Object> nativeTemplate;
+
+	public RedisKeyValueTemplateTests(RedisConnectionFactory connectionFactory) {
+
+		this.connectionFactory = connectionFactory;
+		ConnectionFactoryTracker.add(connectionFactory);
+	}
+
+	@Parameters
+	public static List<RedisConnectionFactory> params() {
+
+		JedisConnectionFactory jedis = new JedisConnectionFactory();
+		jedis.afterPropertiesSet();
+
+		return Collections.<RedisConnectionFactory> singletonList(jedis);
+	}
+
+	@AfterClass
+	public static void cleanUp() {
+		ConnectionFactoryTracker.cleanUp();
+	}
+
+	@Before
+	public void setUp() {
+
+		nativeTemplate = new RedisTemplate<Object, Object>();
+		nativeTemplate.setConnectionFactory(connectionFactory);
+		nativeTemplate.afterPropertiesSet();
+
+		RedisMappingContext context = new RedisMappingContext();
+
+		RedisKeyValueAdapter adapter = new RedisKeyValueAdapter(nativeTemplate, context);
+		template = new RedisKeyValueTemplate(adapter, context);
+	}
+
+	@After
+	public void tearDown() {
+
+		nativeTemplate.execute(new RedisCallback<Void>() {
+
+			@Override
+			public Void doInRedis(RedisConnection connection) throws DataAccessException {
+
+				connection.flushDb();
+				return null;
+			}
+		});
+	}
+
+	/**
+	 * @see DATAREDIS-425
+	 */
+	@Test
+	public void savesObjectCorrectly() {
+
+		final Person rand = new Person();
+		rand.firstname = "rand";
+
+		template.insert(rand);
+
+		nativeTemplate.execute(new RedisCallback<Void>() {
+
+			@Override
+			public Void doInRedis(RedisConnection connection) throws DataAccessException {
+
+				assertThat(connection.exists(("template-test-person:" + rand.id).getBytes()), is(true));
+				return null;
+			}
+		});
+	}
+
+	/**
+	 * @see DATAREDIS-425
+	 */
+	@Test
+	public void findProcessesCallbackReturningSingleIdCorrectly() {
+
+		Person rand = new Person();
+		rand.firstname = "rand";
+
+		final Person mat = new Person();
+		mat.firstname = "mat";
+
+		template.insert(rand);
+		template.insert(mat);
+
+		List<Person> result = template.find(new RedisCallback<byte[]>() {
+
+			@Override
+			public byte[] doInRedis(RedisConnection connection) throws DataAccessException {
+				return mat.id.getBytes();
+			}
+		}, Person.class);
+
+		assertThat(result.size(), is(1));
+		assertThat(result, hasItems(mat));
+	}
+
+	/**
+	 * @see DATAREDIS-425
+	 */
+	@Test
+	public void findProcessesCallbackReturningMultipleIdsCorrectly() {
+
+		final Person rand = new Person();
+		rand.firstname = "rand";
+
+		final Person mat = new Person();
+		mat.firstname = "mat";
+
+		template.insert(rand);
+		template.insert(mat);
+
+		List<Person> result = template.find(new RedisCallback<List<byte[]>>() {
+
+			@Override
+			public List<byte[]> doInRedis(RedisConnection connection) throws DataAccessException {
+				return Arrays.asList(rand.id.getBytes(), mat.id.getBytes());
+			}
+		}, Person.class);
+
+		assertThat(result.size(), is(2));
+		assertThat(result, hasItems(rand, mat));
+	}
+
+	/**
+	 * @see DATAREDIS-425
+	 */
+	@Test
+	public void findProcessesCallbackReturningNullCorrectly() {
+
+		Person rand = new Person();
+		rand.firstname = "rand";
+
+		Person mat = new Person();
+		mat.firstname = "mat";
+
+		template.insert(rand);
+		template.insert(mat);
+
+		List<Person> result = template.find(new RedisCallback<List<byte[]>>() {
+
+			@Override
+			public List<byte[]> doInRedis(RedisConnection connection) throws DataAccessException {
+				return null;
+			}
+		}, Person.class);
+
+		assertThat(result.size(), is(0));
+	}
+
+	@RedisHash("template-test-person")
+	static class Person {
+
+		@Id String id;
+		String firstname;
+
+		@Override
+		public int hashCode() {
+
+			int result = ObjectUtils.nullSafeHashCode(firstname);
+			return result + ObjectUtils.nullSafeHashCode(id);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof Person)) {
+				return false;
+			}
+			Person that = (Person) obj;
+
+			if (!ObjectUtils.nullSafeEquals(this.firstname, that.firstname)) {
+				return false;
+			}
+
+			return ObjectUtils.nullSafeEquals(this.id, that.id);
+		}
+
+	}
+}
