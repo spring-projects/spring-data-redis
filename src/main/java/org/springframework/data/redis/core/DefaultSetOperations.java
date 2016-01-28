e7bfa2f26e699df04d2fc9ef2e475f4edb69ceed
Christoph Strobl <cstrobl@pivotal.io>
2016-01-28 07:59:32 +0100

    DATAREDIS-448 - Fix DefaultSetOperations.intersectAndStore not returning command result.
    We now properly return the number of elements in the destination set, has always been null so far, for DefaultSetOperations.intersectAndStore.

--- src/main/java/org/springframework/data/redis/core/DefaultSetOperations.java
+++ src/main/java/org/springframework/data/redis/core/DefaultSetOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2014 the original author or authors.
+ * Copyright 2011-2016 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -105,8 +105,7 @@ class DefaultSetOperations<K, V> extends AbstractOperations<K, V> implements Set
 		return execute(new RedisCallback<Long>() {
 
 			public Long doInRedis(RedisConnection connection) {
-				connection.sInterStore(rawDestKey, rawKeys);
-				return null;
+				return connection.sInterStore(rawDestKey, rawKeys);
 			}
 		}, true);
 	}
