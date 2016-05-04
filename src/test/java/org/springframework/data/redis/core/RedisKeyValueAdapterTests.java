f2cee266e30253c787fca7a4738420b77a1f0cdb
Christoph Strobl <cstrobl@pivotal.io>
2016-05-04 08:33:23 +0200

    DATAREDIS-507 - Polishing.
    Added missing author and issue reference.
    
    Original Pull Request: #195

--- src/test/java/org/springframework/data/redis/core/RedisKeyValueAdapterTests.java
+++ src/test/java/org/springframework/data/redis/core/RedisKeyValueAdapterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 the original author or authors.
+ * Copyright 2015-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,6 +46,7 @@ import org.springframework.data.redis.core.mapping.RedisMappingContext;
 
 /**
  * @author Christoph Strobl
+ * @author Mark Paluch
  */
 public class RedisKeyValueAdapterTests {
 
