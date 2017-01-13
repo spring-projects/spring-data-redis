14f252f2fd673910bbebf31bcb7146d2a6d5a0d9
Christoph Strobl <cstrobl@pivotal.io>
2017-01-13 08:10:22 +0100

    DATAREDIS-564 - Polishing.
    Update license header.
    Add ticket reference to existing testcase.
    
    Original Pull Request: #228

--- src/test/java/org/springframework/data/redis/connection/jedis/JedisConnectionIntegrationTests.java
+++ src/test/java/org/springframework/data/redis/connection/jedis/JedisConnectionIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 the original author or authors.
+ * Copyright 2011-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.data.redis.connection.jedis;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -361,6 +360,7 @@ public class JedisConnectionIntegrationTests extends AbstractConnectionIntegrati
 
 	/**
 	 * @see DATAREDIS-286
+	 * @see DATAREDIS-564
 	 */
 	@Test
 	public void expireShouldSupportExiprationForValuesLargerThanInteger() {
