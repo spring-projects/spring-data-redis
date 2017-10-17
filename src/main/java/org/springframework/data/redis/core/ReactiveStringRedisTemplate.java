0396923d50d502d05c7916048763dc7f76453ccf
Mark Paluch <mpaluch@pivotal.io>
2017-10-17 16:39:27 +0200

    DATAREDIS-643 - Provide ReactiveStringRedisTemplate.
    Provide a String-focused convenience class for simpler bootstrapping and to provide a dedicated bean type for the application context when using auto-configuration.
    
    Original Pull Request: #288

---
+++ src/main/java/org/springframework/data/redis/core/ReactiveStringRedisTemplate.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.core;
+
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+/**
+ * String-focused extension of {@link ReactiveRedisTemplate}. Since most operations against Redis are {@link String}
+ * based, this class provides a dedicated arrangement that minimizes configuration of its more generic
+ * {@link ReactiveRedisTemplate template} especially in terms of serializers.
+ * 
+ * @author Mark Paluch
+ * @since 2.1
+ */
+public class ReactiveStringRedisTemplate extends ReactiveRedisTemplate<String, String> {
+
+	/**
+	 * Creates new {@link ReactiveRedisTemplate} using given {@link ReactiveRedisConnectionFactory} applying default
+	 * {@link String} serialization.
+	 *
+	 * @param connectionFactory must not be {@literal null}.
+	 * @see RedisSerializationContext#string()
+	 */
+	public ReactiveStringRedisTemplate(ReactiveRedisConnectionFactory connectionFactory) {
+		this(connectionFactory, RedisSerializationContext.string());
+	}
+
+	/**
+	 * Creates new {@link ReactiveRedisTemplate} using given {@link ReactiveRedisConnectionFactory} and
+	 * {@link RedisSerializationContext}.
+	 *
+	 * @param connectionFactory must not be {@literal null}.
+	 * @param serializationContext must not be {@literal null}.
+	 */
+	public ReactiveStringRedisTemplate(ReactiveRedisConnectionFactory connectionFactory,
+			RedisSerializationContext<String, String> serializationContext) {
+		super(connectionFactory, serializationContext);
+	}
+
+	/**
+	 * Creates new {@link ReactiveRedisTemplate} using given {@link ReactiveRedisConnectionFactory} and
+	 * {@link RedisSerializationContext}.
+	 *
+	 * @param connectionFactory must not be {@literal null}.
+	 * @param serializationContext must not be {@literal null}.
+	 * @param exposeConnection flag indicating to expose the connection used.
+	 */
+	public ReactiveStringRedisTemplate(ReactiveRedisConnectionFactory connectionFactory,
+			RedisSerializationContext<String, String> serializationContext, boolean exposeConnection) {
+		super(connectionFactory, serializationContext, exposeConnection);
+	}
+}
---
+++ src/test/java/org/springframework/data/redis/core/ReactiveStringRedisTemplateIntegrationTests.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.core;
+
+import reactor.test.StepVerifier;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.data.redis.ConnectionFactoryTracker;
+import org.springframework.data.redis.SettingsUtils;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceTestClientConfiguration;
+
+/**
+ * Integration tests for {@link ReactiveStringRedisTemplate}.
+ * 
+ * @author Mark Paluch
+ */
+public class ReactiveStringRedisTemplateIntegrationTests {
+
+	private static ReactiveRedisConnectionFactory connectionFactory;
+
+	private ReactiveStringRedisTemplate template;
+
+	@BeforeClass
+	public static void beforeClass() {
+
+		LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(SettingsUtils.standaloneConfiguration(),
+				LettuceTestClientConfiguration.create());
+		connectionFactory.afterPropertiesSet();
+		ConnectionFactoryTracker.add(connectionFactory);
+
+		ReactiveStringRedisTemplateIntegrationTests.connectionFactory = connectionFactory;
+	}
+
+	@Before
+	public void before() {
+
+		template = new ReactiveStringRedisTemplate(connectionFactory);
+
+		StepVerifier.create(template.execute(connection -> connection.serverCommands().flushDb())).expectNext("OK")
+				.verifyComplete();
+	}
+
+	@Test // DATAREDIS-643
+	public void shouldSetAndGetKeys() {
+
+		StepVerifier.create(template.opsForValue().set("key", "value")).expectNext(true).verifyComplete();
+		StepVerifier.create(template.opsForValue().get("key")).expectNext("value").verifyComplete();
+	}
+}
