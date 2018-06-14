4c971587594a9191e7e78121caaea2fe7f3235d6
Mark Paluch <mpaluch@pivotal.io>
2018-06-14 16:01:25 +0200

    DATAREDIS-708 - Move BGREWRITEAOF and BGSAVE tests to unit tests.
    Run tests against Lettuce connection mocks to not interfere with running Redis background processes.

---
+++ src/test/java/org/springframework/data/redis/connection/lettuce/LettuceReactiveRedisClusterConnectionUnitTests.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.connection.lettuce;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.springframework.data.redis.connection.ClusterTestVariables.*;
+
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.reactive.RedisAdvancedClusterReactiveCommands;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.redis.connection.RedisClusterNode;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionProvider.TargetAware;
+
+/**
+ * Unit tests for {@link LettuceReactiveRedisClusterConnection}.
+ *
+ * @author Mark Paluch
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LettuceReactiveRedisClusterConnectionUnitTests {
+
+	static final RedisClusterNode NODE1 = new RedisClusterNode(CLUSTER_HOST, MASTER_NODE_1_PORT);
+
+	@Mock StatefulRedisClusterConnection<ByteBuffer, ByteBuffer> sharedConnection;
+	@Mock StatefulRedisConnection<ByteBuffer, ByteBuffer> nodeConnection;
+
+	@Mock RedisClusterClient clusterClient;
+	@Mock RedisAdvancedClusterReactiveCommands<ByteBuffer, ByteBuffer> reactiveCommands;
+	@Mock RedisReactiveCommands<ByteBuffer, ByteBuffer> reactiveNodeCommands;
+	@Mock(extraInterfaces = TargetAware.class) LettuceConnectionProvider connectionProvider;
+
+	@Before
+	public void before() {
+
+		when(connectionProvider.getConnection(any())).thenReturn(sharedConnection);
+		when(sharedConnection.getConnection(anyString(), anyInt())).thenReturn(nodeConnection);
+		when(nodeConnection.reactive()).thenReturn(reactiveNodeCommands);
+	}
+
+	@Test // DATAREDIS-659, DATAREDIS-708
+	public void bgReWriteAofShouldRespondCorrectly() {
+
+		LettuceReactiveRedisClusterConnection connection = new LettuceReactiveRedisClusterConnection(connectionProvider,
+				clusterClient);
+
+		when(reactiveNodeCommands.bgrewriteaof()).thenReturn(Mono.just("OK"));
+
+		StepVerifier.create(connection.serverCommands().bgReWriteAof(NODE1)).expectNextCount(1).verifyComplete();
+	}
+
+	@Test // DATAREDIS-659, DATAREDIS-708
+	public void bgSaveShouldRespondCorrectly() {
+
+		LettuceReactiveRedisClusterConnection connection = new LettuceReactiveRedisClusterConnection(connectionProvider,
+				clusterClient);
+
+		when(reactiveNodeCommands.bgsave()).thenReturn(Mono.just("OK"));
+
+		StepVerifier.create(connection.serverCommands().bgSave(NODE1)).expectNextCount(1).verifyComplete();
+	}
+}
