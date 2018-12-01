8ecd31ce295c120684b66d1a2b3271cff5653b8b
chao chang <chang-chao@users.noreply.github.com>
2018-12-01 09:10:47 +0900

    DATAREDIS-903 - Remove Javadoc for unused RedisClusterConfiguration settings.
    The following configuration keys which are not used in RedisClusterConfiguration, are removed from the Javadoc.
    	 * spring.redis.cluster.timeout=5
    	 * spring.redis.cluster.password=foobar
    
    Original pull request: #375.

--- src/main/java/org/springframework/data/redis/connection/RedisClusterConfiguration.java
+++ src/main/java/org/springframework/data/redis/connection/RedisClusterConfiguration.java
@@ -80,9 +80,7 @@ public class RedisClusterConfiguration {
 	 * <pre>
 	 * <code>
 	 * spring.redis.cluster.nodes=127.0.0.1:23679,127.0.0.1:23680,127.0.0.1:23681
-	 * spring.redis.cluster.timeout=5
 	 * spring.redis.cluster.max-redirects=3
-	 * spring.redis.cluster.password=foobar
 	 * </code>
 	 * </pre>
 	 *
