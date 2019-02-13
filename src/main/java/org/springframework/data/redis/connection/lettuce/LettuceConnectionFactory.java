99508cb332ad903b14bac81ce71b59cd8d46af29
Luis De Bello <luis.debello@mulesoft.com>
2019-02-13 01:00:01 -0300

    DATAREDIS-930 - Return port/host of WithHostAndPort configurations using LettuceConnectionFactory.getPort() and getHost().
    We now correctly return host and port from configurations that implement WithHostAndPort instead of always using RedisStandaloneConfiguration.
    
    Original pull request: #385.

--- src/main/java/org/springframework/data/redis/connection/lettuce/LettuceConnectionFactory.java
+++ src/main/java/org/springframework/data/redis/connection/lettuce/LettuceConnectionFactory.java
@@ -86,6 +86,7 @@ import org.springframework.util.ClassUtils;
  * @author Mark Paluch
  * @author Balázs Németh
  * @author Ruben Cervilla
+ * @author Luis De Bello
  */
 public class LettuceConnectionFactory
 		implements InitializingBean, DisposableBean, RedisConnectionFactory, ReactiveRedisConnectionFactory {
@@ -465,7 +466,7 @@ public class LettuceConnectionFactory
 	 * @return the host.
 	 */
 	public String getHostName() {
-		return standaloneConfig.getHostName();
+		return RedisConfiguration.getHostOrElse(configuration, standaloneConfig::getHostName);
 	}
 
 	/**
@@ -485,7 +486,7 @@ public class LettuceConnectionFactory
 	 * @return the port.
 	 */
 	public int getPort() {
-		return standaloneConfig.getPort();
+		return RedisConfiguration.getPortOrElse(configuration, standaloneConfig::getPort);
 	}
 
 	/**
