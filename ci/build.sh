986e3b1ea36ee207744a8c87a3ce646e4ff1a925
Greg Turnquist <gturnquist@pivotal.io>
2019-02-28 08:04:39 -0600

    DATAREDIS-920 - Fix path to artifactory.

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
-spring_data_redis_artifactory=$(pwd)/spring_data_redis_artifactory
+spring_data_redis_artifactory=$(pwd)/spring-data-redis-artifactory
 
 rm -rf $HOME/.m2/repository/org/springframework/data 2> /dev/null || :
 
