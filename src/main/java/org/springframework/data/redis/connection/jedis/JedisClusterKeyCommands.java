be8c40b1eb54ba4b8d29587cfe317c039cccb604
Mark Paluch <mpaluch@pivotal.io>
2020-08-10 11:43:57 +0200

    DATAREDIS-1190 - Cluster rename now correctly overwrites existing keys.
    rename(…) behavior in cluster-mode is now aligned with standalone Redis behavior that overwrites the target key if it already exists and the renamed key uses a different slot. Previously, the underlying restore command was called without the replace option which caused BUSYKEY failures.
    
    Original Pull Request: #555

--- src/main/java/org/springframework/data/redis/connection/jedis/JedisClusterKeyCommands.java
+++ src/main/java/org/springframework/data/redis/connection/jedis/JedisClusterKeyCommands.java
@@ -269,7 +269,7 @@ class JedisClusterKeyCommands implements RedisKeyCommands {
 
 		if (value != null && value.length > 0) {
 
-			restore(targetKey, 0, value);
+			restore(targetKey, 0, value, true);
 			del(sourceKey);
 		}
 	}
