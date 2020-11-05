272498ec0cbe753fe8ba304c7e83f2b5da17233a
Mark Paluch <mpaluch@pivotal.io>
2020-11-05 10:44:53 +0100

    DATAREDIS-1245 - Consider List subtypes used with RedisScript as MULTI return type.
    We now consider List and subtypes of List to be returned as MULTI return type. Previously, List subtypes were considered to represent the value type.

---
+++ src/test/java/org/springframework/data/redis/connection/ReturnTypeUnitTests.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.connection;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/**
+ * Unit tests for {@link ReturnType}.
+ *
+ * @author Mark Paluch
+ */
+class ReturnTypeUnitTests {
+
+	@ParameterizedTest // DATAREDIS-1245
+	@ValueSource(classes = { List.class, ArrayList.class, LinkedList.class })
+	void shouldConsiderListsAsMultiType(Class<?> listClass) {
+
+		assertThat(ReturnType.fromJavaType(listClass)).isEqualTo(ReturnType.MULTI);
+	}
+
+}
