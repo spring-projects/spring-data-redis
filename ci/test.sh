3e355534c4b0056436ec09e247f6fa6db9ca8577
Greg L. Turnquist <gturnquist@vmware.com>
2020-12-16 09:36:55 -0600

    DATAREDIS-1258 - Polishing.

---
+++ ci/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+set -euo pipefail
+
+rm -f work
+
+# Create link to directory with Redis binaries
+cwd=$(pwd)
+
+# Launch Redis in proper configuration
+pushd /tmp && ln -s /work && make -f $cwd/Makefile start && popd
+
+# Execute maven test
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw clean test -P${PROFILE} -DrunLongTests=${LONG_TESTS:-false} -B
+
+# Capture resulting exit code from maven (pass/fail)
+RESULT=$?
+
+# Shutdown Redis
+pushd /tmp && make -f $cwd/Makefile stop && popd
+
+# Return maven results
+exit $RESULT
