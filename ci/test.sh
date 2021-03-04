33bfdb1b88d87b858ed15d719b69623b657f8e8b
Christoph Strobl <cstrobl@vmware.com>
2021-03-04 14:29:15 +0100

    Remove 6.0 Dockerfiles

--- ci/test.sh
+++ ci/test.sh
@@ -8,7 +8,7 @@ rm -f work
 cwd=$(pwd)
 
 # Launch Redis in proper configuration
-pushd /tmp && ln -s /work && make -f $cwd/Makefile start REDIS_VERSION=6.2.1 && popd
+pushd /tmp && ln -s /work && make -f $cwd/Makefile start && popd
 
 # Execute maven test
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw clean test -P${PROFILE} -DrunLongTests=${LONG_TESTS:-false} -B
