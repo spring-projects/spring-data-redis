e5c8a6ba18034780129fc8ec3c7b33f13ea2df21
Dylan <quantum64development@gmail.com>
2022-01-07 02:04:29 -0500

    Add approximate trimming to reactive stream extensions

--- src/main/kotlin/org/springframework/data/redis/core/ReactiveStreamOperationsExtensions.kt
+++ src/main/kotlin/org/springframework/data/redis/core/ReactiveStreamOperationsExtensions.kt
@@ -265,5 +265,6 @@ inline fun <K : Any, reified V : Any> ReactiveStreamOperations<K, *, *>.reverseR
  * @author Mark Paluch
  * @since 2.2
  */
-suspend fun <K : Any, HK : Any, HV : Any> ReactiveStreamOperations<K, HK, HV>.trimAndAwait(key: K, count: Long): Long =
-		trim(key, count).awaitSingle()
+@JvmOverloads // Maintain compatibility with versions <= 2.6.0
+suspend fun <K : Any, HK : Any, HV : Any> ReactiveStreamOperations<K, HK, HV>.trimAndAwait(key: K, count: Long, approximateTrimming: Boolean = false): Long =
+		trim(key, count, approximateTrimming).awaitSingle()
