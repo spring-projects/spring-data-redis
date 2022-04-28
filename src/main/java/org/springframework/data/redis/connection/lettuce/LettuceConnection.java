0712b435083088978a2415ccd14c55fdec67d9b3
skyjiang <470623352@qq.com>
2022-04-28 16:50:26 +0800

    remove duplicate map key of TypeHints

--- src/main/java/org/springframework/data/redis/connection/lettuce/LettuceConnection.java
+++ src/main/java/org/springframework/data/redis/connection/lettuce/LettuceConnection.java
@@ -1038,7 +1038,6 @@ public class LettuceConnection extends AbstractRedisConnection {
 			COMMAND_OUTPUT_TYPE_MAPPING.put(DECR, IntegerOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(DECRBY, IntegerOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(DEL, IntegerOutput.class);
-			COMMAND_OUTPUT_TYPE_MAPPING.put(COPY, IntegerOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(GETBIT, IntegerOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(HDEL, IntegerOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(HINCRBY, IntegerOutput.class);
@@ -1081,7 +1080,6 @@ public class LettuceConnection extends AbstractRedisConnection {
 			// DOUBLE
 			COMMAND_OUTPUT_TYPE_MAPPING.put(HINCRBYFLOAT, DoubleOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(INCRBYFLOAT, DoubleOutput.class);
-			COMMAND_OUTPUT_TYPE_MAPPING.put(MGET, ValueListOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(ZINCRBY, DoubleOutput.class);
 			COMMAND_OUTPUT_TYPE_MAPPING.put(ZSCORE, DoubleOutput.class);
 
