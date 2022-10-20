db74402b8c8ec1076ab43061a4787d8a51cabf53
Mark Paluch <mpaluch@vmware.com>
2022-10-20 10:33:58 +0200

    Add observability adapter for Lettuce.
    We now provide MicrometerTracingAdapter to connect Lettuce to Micrometer Tracing.
    
    Closes #2348

---
+++ src/main/java/org/springframework/data/redis/connection/lettuce/observability/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Integration of Micrometer Tracing for Lettuce Observability.
+ */
+@org.springframework.lang.NonNullApi
+@org.springframework.lang.NonNullFields
+package org.springframework.data.redis.connection.lettuce.observability;
