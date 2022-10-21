e99463c4d90a868ec2bbb76c99b74233040c2a46
Christoph Strobl <cstrobl@vmware.com>
2022-10-21 14:12:54 +0200

    Add missing RedisClient runtime hint.
    Closes: #2440

---
+++ src/main/java/org/springframework/data/redis/connection/lettuce/aot/LettuceRuntimeHints.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.redis.connection.lettuce.aot;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.lang.Nullable;
+import org.springframework.util.ClassUtils;
+
+/**
+ * @author Christoph Strobl
+ * @since 3.0
+ */
+class LettuceRuntimeHints implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+
+		if (ClassUtils.isPresent("io.lettuce.core.RedisClient", classLoader)) {
+
+			hints.reflection().registerType(TypeReference.of("io.lettuce.core.RedisClient"),
+					it -> it
+							.onReachableType(
+									TypeReference.of("org.springframework.data.redis.connection.lettuce.StandaloneConnectionProvider"))
+							.withMembers(MemberCategory.INVOKE_PUBLIC_METHODS, MemberCategory.DECLARED_FIELDS));
+		}
+	}
+}
