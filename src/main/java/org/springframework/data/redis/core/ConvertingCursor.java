f066d6bac9728d4a2c2629d2eff61e95adf84f4b
Mingyuan Wu <my.wu@outlook.com>
2023-08-31 16:37:28 +0800

    Fix wrong asserted code in ConvertingCursor.
    Pull Request: #2696

--- src/main/java/org/springframework/data/redis/core/ConvertingCursor.java
+++ src/main/java/org/springframework/data/redis/core/ConvertingCursor.java
@@ -41,7 +41,7 @@ public class ConvertingCursor<S, T> implements Cursor<T> {
 	public ConvertingCursor(Cursor<S> cursor, Converter<S, T> converter) {
 
 		Assert.notNull(cursor, "Cursor delegate must not be 'null'");
-		Assert.notNull(cursor, "Converter must not be 'null'");
+		Assert.notNull(converter, "Converter must not be 'null'");
 		this.delegate = cursor;
 		this.converter = converter;
 	}
