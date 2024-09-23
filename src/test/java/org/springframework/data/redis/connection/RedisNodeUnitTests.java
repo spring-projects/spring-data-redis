4a5214e6e827d6ed7d215e69c5d1b3769ea28c0e
LeeHyungGeol <rjf1138@naver.com>
2024-09-23 21:18:44 +0900

    Add comment about Apache License & author in the headers of the class.

--- src/test/java/org/springframework/data/redis/connection/RedisNodeUnitTests.java
+++ src/test/java/org/springframework/data/redis/connection/RedisNodeUnitTests.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2014-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.redis.connection;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
+/**
+ * Unit tests for {@link RedisNode}.
+ * referred to the test code in ConversionUnitTests.java
+ *
+ * @author LeeHyungGeol
+ */
 public class RedisNodeUnitTests {
 	private static final int DEFAULT_PORT = 6379;
 
