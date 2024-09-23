b1beb9768b1c14580ab10f5c6673f1d359062c44
Jinbeom <kst20026@gamil.com>
2024-09-23 10:10:29 +0900

    Refactor afterPropertiesSet() method using Objects.requireNonNullElse.

--- src/main/java/org/springframework/data/redis/core/RedisTemplate.java
+++ src/main/java/org/springframework/data/redis/core/RedisTemplate.java
@@ -17,6 +17,7 @@ package org.springframework.data.redis.core;
 
 import java.io.Closeable;
 import java.lang.reflect.Proxy;
+import java.util.Objects;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -126,34 +127,25 @@ public class RedisTemplate<K, V> extends RedisAccessor implements RedisOperation
 
 	@Override
 	public void afterPropertiesSet() {
-
 		super.afterPropertiesSet();
 
-		if (defaultSerializer == null) {
-
-			defaultSerializer = new JdkSerializationRedisSerializer(
-					classLoader != null ? classLoader : this.getClass().getClassLoader());
-		}
+		defaultSerializer = Objects.requireNonNullElseGet(
+				defaultSerializer,
+				() -> new JdkSerializationRedisSerializer(
+						Objects.requireNonNullElse(classLoader, getClass().getClassLoader()))
+		);
 
 		if (enableDefaultSerializer) {
-
-			if (keySerializer == null) {
-				keySerializer = defaultSerializer;
-			}
-			if (valueSerializer == null) {
-				valueSerializer = defaultSerializer;
-			}
-			if (hashKeySerializer == null) {
-				hashKeySerializer = defaultSerializer;
-			}
-			if (hashValueSerializer == null) {
-				hashValueSerializer = defaultSerializer;
-			}
+			keySerializer = Objects.requireNonNullElse(keySerializer, defaultSerializer);
+			valueSerializer = Objects.requireNonNullElse(valueSerializer, defaultSerializer);
+			hashKeySerializer = Objects.requireNonNullElse(hashKeySerializer, defaultSerializer);
+			hashValueSerializer = Objects.requireNonNullElse(hashValueSerializer, defaultSerializer);
 		}
 
-		if (scriptExecutor == null) {
-			this.scriptExecutor = new DefaultScriptExecutor<>(this);
-		}
+		scriptExecutor = Objects.requireNonNullElseGet(
+				scriptExecutor,
+				() -> new DefaultScriptExecutor<>(this)
+		);
 
 		initialized = true;
 	}
