ec2b6c6c0fd28a6577a373276b4169d4f05e4eed
LeeHyungGeol <rjf1138@naver.com>
2024-09-23 21:13:49 +0900

    Update RedisNodeUnitTests code lint to Intellij Default Settings.

--- src/test/java/org/springframework/data/redis/connection/RedisNodeUnitTests.java
+++ src/test/java/org/springframework/data/redis/connection/RedisNodeUnitTests.java
@@ -1,1 +1,51 @@
-package org.springframework.data.redis.connection;import static org.assertj.core.api.Assertions.*;import org.junit.jupiter.api.Test;public class RedisNodeUnitTests {  private static final int DEFAULT_PORT = 6379;  @Test // GH-2928  void shouldParseIPv4AddressWithPort() {	RedisNode node = RedisNode.fromString("127.0.0.1:6379");	assertThat(node.getHost()).isEqualTo("127.0.0.1");	assertThat(node.getPort()).isEqualTo(6379);  }  @Test // GH-2928  void shouldParseIPv6AddressWithPort() {	RedisNode node = RedisNode.fromString("[aaaa:bbbb::dddd:eeee]:6379");	assertThat(node.getHost()).isEqualTo("aaaa:bbbb::dddd:eeee");	assertThat(node.getPort()).isEqualTo(6379);  }  @Test // GH-2928  void shouldParseBareHostnameWithPort() {	RedisNode node = RedisNode.fromString("my.redis.server:6379");	assertThat(node.getHost()).isEqualTo("my.redis.server");	assertThat(node.getPort()).isEqualTo(6379);  }  @Test // GH-2928  void shouldThrowExceptionForInvalidPort() {	assertThatIllegalArgumentException()		.isThrownBy(() -> RedisNode.fromString("127.0.0.1:invalidPort"));  }  @Test // GH-2928  void shouldParseBareIPv6WithoutPort() {	RedisNode node = RedisNode.fromString("2001:0db8:85a3:0000:0000:8a2e:0370:7334");	assertThat(node.getHost()).isEqualTo("2001:0db8:85a3:0000:0000:8a2e:0370");	assertThat(node.getPort()).isEqualTo(7334);  }  @Test // GH-2928  void shouldParseBareHostnameWithoutPort() {	RedisNode node = RedisNode.fromString("my.redis.server");	assertThat(node.getHost()).isEqualTo("my.redis.server");	assertThat(node.getPort()).isEqualTo(DEFAULT_PORT);  }}
+package org.springframework.data.redis.connection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RedisNodeUnitTests {
+	private static final int DEFAULT_PORT = 6379;
+
+	@Test // GH-2928
+	void shouldParseIPv4AddressWithPort() {
+		RedisNode node = RedisNode.fromString("127.0.0.1:6379");
+		assertThat(node.getHost()).isEqualTo("127.0.0.1");
+		assertThat(node.getPort()).isEqualTo(6379);
+	}
+
+	@Test // GH-2928
+	void shouldParseIPv6AddressWithPort() {
+		RedisNode node = RedisNode.fromString("[aaaa:bbbb::dddd:eeee]:6379");
+		assertThat(node.getHost()).isEqualTo("aaaa:bbbb::dddd:eeee");
+		assertThat(node.getPort()).isEqualTo(6379);
+	}
+
+	@Test // GH-2928
+	void shouldParseBareHostnameWithPort() {
+		RedisNode node = RedisNode.fromString("my.redis.server:6379");
+		assertThat(node.getHost()).isEqualTo("my.redis.server");
+		assertThat(node.getPort()).isEqualTo(6379);
+	}
+
+	@Test // GH-2928
+	void shouldThrowExceptionForInvalidPort() {
+		assertThatIllegalArgumentException()
+			.isThrownBy(() -> RedisNode.fromString("127.0.0.1:invalidPort"));
+	}
+
+	@Test // GH-2928
+	void shouldParseBareIPv6WithoutPort() {
+		RedisNode node = RedisNode.fromString("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+		assertThat(node.getHost()).isEqualTo("2001:0db8:85a3:0000:0000:8a2e:0370");
+		assertThat(node.getPort()).isEqualTo(7334);
+	}
+
+	@Test // GH-2928
+	void shouldParseBareHostnameWithoutPort() {
+		RedisNode node = RedisNode.fromString("my.redis.server");
+		assertThat(node.getHost()).isEqualTo("my.redis.server");
+		assertThat(node.getPort()).isEqualTo(DEFAULT_PORT);
+	}
+}
+
