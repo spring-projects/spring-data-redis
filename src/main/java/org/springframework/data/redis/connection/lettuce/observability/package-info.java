f97b6826eb8a48448de0e8174b390da9a55696e4
Mark Paluch <mark.paluch@broadcom.com>
2025-01-16 11:50:08 +0100

    Remove `MicrometerTracingAdapter` in favor of Lettuce's Micrometer support.
    Closes #3093

--- src/main/java/org/springframework/data/redis/connection/lettuce/observability/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Integration of Micrometer Tracing for Lettuce Observability.
- */
-@org.springframework.lang.NonNullApi
-@org.springframework.lang.NonNullFields
-package org.springframework.data.redis.connection.lettuce.observability;
