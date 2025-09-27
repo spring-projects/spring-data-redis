8b5b266cd47ba27540b38c64a1e07d54f7c40824
LeeHyungGeol <rjf1138@naver.com>
2025-09-27 16:53:06 +0900

    Add regression test in TransactionalStringRedisTemplateTests.java & Add author tags in the class header
    Signed-off-by: LeeHyungGeol <rjf1138@naver.com>

--- src/main/java/org/springframework/data/redis/core/RedisCommand.java
+++ src/main/java/org/springframework/data/redis/core/RedisCommand.java
@@ -35,6 +35,7 @@ import org.springframework.util.StringUtils;
  * @author Oscar Cai
  * @author Sébastien Volle
  * @author John Blum
+ * @author LeeHyungGeol
  * @since 1.3
  * @link <a href=
  *       "https://github.com/antirez/redis/blob/843de8b786562d8d77c78d83a971060adc61f77a/src/server.c#L180">Redis
