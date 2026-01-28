38e4ed442e124a0a8db555cca4df7b2808ebc0b4
Ilyass-Bougati <i.bougati12@gmail.com>
2026-01-28 15:16:37 +0100

    GH-1004: Add author attribution and update copyright headers
    Signed-off-by: Ilyass-Bougati <i.bougati12@gmail.com>

--- src/main/java/org/springframework/data/redis/annotation/MessagingMessageListenerAdapter.java
+++ src/main/java/org/springframework/data/redis/annotation/MessagingMessageListenerAdapter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.redis.annotation;
 
 import java.lang.reflect.Method;
@@ -8,6 +23,18 @@ import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.util.ReflectionUtils;
 
+/**
+ * An adapter that delegates {@link MessageListener#onMessage} to a target method via reflection.
+ *
+ * <p>Supports argument resolution for:
+ * <ul>
+ * <li>{@link Message} (The raw Redis message)</li>
+ * <li>{@code byte[]} (The message body)</li>
+ * <li>{@link String} (The message body decoded as UTF-8)</li>
+ * </ul>
+ *
+ * @author Ilyass Bougati
+ */
 public class MessagingMessageListenerAdapter implements MessageListener {
 
 	private Object bean;
--- src/test/java/org/springframework/data/redis/annotation/MessagingMessageListenerAdapterTest.java
+++ src/test/java/org/springframework/data/redis/annotation/MessagingMessageListenerAdapterTest.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.redis.annotation;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.cache.DefaultRedisCacheWriter;
 import org.springframework.data.redis.connection.Message;
 
 import java.lang.reflect.Method;
@@ -12,6 +28,11 @@ import java.nio.charset.StandardCharsets;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
 
+/**
+ * Unit tests for {@link MessagingMessageListenerAdapter}
+ *
+ * @author Ilyass Bougati
+ */
 @ExtendWith(MockitoExtension.class)
 public class MessagingMessageListenerAdapterTest {
 	@Mock private Message message;
