0f3281871498d3689b03f0556f2c9d71a08d019e
Dongliang Xie <dragonfsky@gmail.com>
2026-06-24 23:36:35 +0800

    Remove integer range checks for Jedis TTL.
    Jedis accepts long, so the integer check is not needed.
    
    Original pull request: #3388
    Closes #3386
    
    Signed-off-by: Dongliang Xie <dragonfsky@gmail.com>

--- src/test/java/org/springframework/data/redis/connection/jedis/JedisConnectionUnitTests.java
+++ src/test/java/org/springframework/data/redis/connection/jedis/JedisConnectionUnitTests.java
@@ -20,10 +20,12 @@ import static org.mockito.Mockito.*;
 import static org.springframework.data.redis.test.util.IntRangeAssertions.*;
 
 import redis.clients.jedis.AbstractTransaction;
+import redis.clients.jedis.CommandArguments;
 import redis.clients.jedis.CommandObject;
 import redis.clients.jedis.Connection;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.UnifiedJedis;
+import redis.clients.jedis.args.Rawable;
 
 import java.io.IOException;
 
@@ -49,6 +51,7 @@ import org.springframework.data.redis.connection.RedisServerCommands.ShutdownOpt
  * @author Tihomir Mateev
  * @author Tiefang Hu
  * @author Moritz Halbritter
+ * @author Dongliang Xie
  */
 class JedisConnectionUnitTests {
 
@@ -110,21 +113,40 @@ class JedisConnectionUnitTests {
 		}
 
 		/**
-		 * Captures the CommandObject sent via executeCommand and returns a string containing
-		 * the command name and all arguments.
+		 * Captures the command sent to the mock connection and returns a string containing the
+		 * command name and all arguments.
 		 */
 		@SuppressWarnings("unchecked")
 		private String captureCommand() {
+
+			if (connection.isPipelined()) {
+				ArgumentCaptor<CommandArguments> captor = ArgumentCaptor.forClass(CommandArguments.class);
+				verify(connectionMock, atLeastOnce()).sendCommand(captor.capture());
+				return commandToString(captor.getValue());
+			}
+
 			ArgumentCaptor<CommandObject<?>> captor = ArgumentCaptor.forClass(CommandObject.class);
 			verify(connectionMock, atLeastOnce()).executeCommand(captor.capture());
-			CommandObject<?> lastCommand = captor.getValue();
-			// Build a string from all raw arguments
+			return commandToString(captor.getValue().getArguments());
+		}
+
+		private String commandToString(Iterable<? extends Rawable> arguments) {
+
 			StringBuilder sb = new StringBuilder();
-			for (var arg : lastCommand.getArguments()) {
-				if (sb.length() > 0) sb.append(" ");
+
+			for (Rawable arg : arguments) {
+				if (sb.length() > 0) {
+					sb.append(" ");
+				}
 				sb.append(new String(arg.getRaw()));
 			}
+
 			return sb.toString();
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		private void returnOkForCommands() {
+			when(connectionMock.executeCommand(any(CommandObject.class))).thenReturn("OK");
 		}
 
 		@Test // DATAREDIS-184, GH-2153
@@ -215,16 +237,30 @@ class JedisConnectionUnitTests {
 					.isThrownBy(() -> connection.getSentinelConnection());
 		}
 
-		@Test // DATAREDIS-472
-		void restoreShouldThrowExceptionWhenTtlInMillisExceedsIntegerRange() {
-			assertRejectsOutOfIntRange("TtlInMillis for restore in Jedis",
-					(ttl) -> connection.restore("foo".getBytes(), ttl, "bar".getBytes()));
+		@Test // GH-3386
+		void restoreShouldPassLongTtlToJedis() {
+
+			long ttlInMillis = (long) Integer.MAX_VALUE + 1L;
+
+			returnOkForCommands();
+
+			connection.restore("foo".getBytes(), ttlInMillis, "bar".getBytes());
+
+			String command = captureCommand();
+			assertThat(command).isEqualTo("RESTORE foo " + ttlInMillis + " bar");
 		}
 
-		@Test // GH-3437
-		void restoreWithReplaceShouldThrowExceptionWhenTtlInMillisExceedsIntegerRange() {
-			assertRejectsOutOfIntRange("TtlInMillis for restore in Jedis",
-					(ttl) -> connection.restore("foo".getBytes(), ttl, "bar".getBytes(), true));
+		@Test // GH-3386
+		void restoreWithReplaceShouldPassLongTtlToJedis() {
+
+			long ttlInMillis = (long) Integer.MAX_VALUE + 1L;
+
+			returnOkForCommands();
+
+			connection.restore("foo".getBytes(), ttlInMillis, "bar".getBytes(), true);
+
+			String command = captureCommand();
+			assertThat(command).isEqualTo("RESTORE foo " + ttlInMillis + " bar REPLACE");
 		}
 
 		@Test // GH-3438
