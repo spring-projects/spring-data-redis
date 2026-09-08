3e50b660ee337953af45886680861ac5302f2294
Moritz Halbritter <moritz.halbritter@broadcom.com>
2026-09-08 10:02:34 +0200

    Fix wrong assertion for XACKDEL tests
    See #3282
    Original Pull Request #3295

--- src/test/java/org/springframework/data/redis/connection/lettuce/LettuceReactiveStreamCommandsIntegrationTests.java
+++ src/test/java/org/springframework/data/redis/connection/lettuce/LettuceReactiveStreamCommandsIntegrationTests.java
@@ -731,7 +731,7 @@ public class LettuceReactiveStreamCommandsIntegrationTests extends LettuceReacti
 				.xReadGroup(Consumer.from("my-group", "my-consumer"),
 						StreamOffset.create(KEY_1_BBUFFER, ReadOffset.lastConsumed())) //
 				.as(StepVerifier::create) //
-				.expectNextCount(1) //
+				.expectNextCount(2) //
 				.verifyComplete();
 
 		XDelOptions options = XDelOptions.deletionPolicy(RedisStreamCommands.StreamDeletionPolicy.removeAcknowledged());
@@ -761,7 +761,7 @@ public class LettuceReactiveStreamCommandsIntegrationTests extends LettuceReacti
 				.xReadGroup(Consumer.from("my-group", "my-consumer"),
 						StreamOffset.create(KEY_1_BBUFFER, ReadOffset.lastConsumed())) //
 				.as(StepVerifier::create) //
-				.expectNextCount(1) //
+				.expectNextCount(2) //
 				.verifyComplete();
 
 		XDelOptions options = XDelOptions.deletionPolicy(RedisStreamCommands.StreamDeletionPolicy.removeAcknowledged());
