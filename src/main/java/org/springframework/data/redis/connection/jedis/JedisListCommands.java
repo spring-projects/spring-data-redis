cdbddcd6cf76df37eaa14d7560043542d96452be
Moritz Halbritter <moritz.halbritter@broadcom.com>
2026-09-08 16:05:26 +0200

    Check for integer overflow in lPop and rPop.
    Closes #3438

--- src/main/java/org/springframework/data/redis/connection/jedis/JedisListCommands.java
+++ src/main/java/org/springframework/data/redis/connection/jedis/JedisListCommands.java
@@ -199,6 +199,10 @@ class JedisListCommands implements RedisListCommands {
 
 		Assert.notNull(key, "Key must not be null");
 
+		if (count > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Count must be less than Integer.MAX_VALUE for lPop in Jedis");
+		}
+
 		return connection.invoke().just(JedisBinaryCommands::lpop, PipelineBinaryCommands::lpop, key, (int) count);
 	}
 
@@ -214,6 +218,10 @@ class JedisListCommands implements RedisListCommands {
 	public List<byte @NonNull []> rPop(byte @NonNull [] key, long count) {
 
 		Assert.notNull(key, "Key must not be null");
+
+		if (count > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Count must be less than Integer.MAX_VALUE for rPop in Jedis");
+		}
 
 		return connection.invoke().just(JedisBinaryCommands::rpop, PipelineBinaryCommands::rpop, key, (int) count);
 	}
